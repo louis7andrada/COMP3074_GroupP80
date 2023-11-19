ffacb9c61aaa0484f40b706d8cc4cfec5c0afe8b
Jerome Delos-Reyes <delosreyes.jerome@proton.me>
2023-11-19 14:15:52 -0500

    added stars icon

--- components/ListItem.jsx
+++ components/ListItem.jsx
@@ -1,8 +1,26 @@
+import React from "react";
 import { Image, StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import Row from "./Row";
 import ResturantIcon from "../assets/restaurant-icon.png";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 export default function ListItem(props) {
+  const renderStars = () => {
+    const stars = [];
+    for (let i = 0; i < props.item.stars; i++) {
+      stars.push(
+        <FontAwesomeIcon
+          key={i}
+          icon={faStar}
+          size={20}
+          color="gold"
+          style={{ marginRight: 5 }}
+        />
+      );
+    }
+    return stars;
+  };
   return (
     <TouchableOpacity
       key={props.item.id}
@@ -11,7 +29,7 @@ export default function ListItem(props) {
       }}
     >
       <Row>
-        <Image source={ResturantIcon} style={{ height: 100, width: 100 }} />
+        <Image source={ResturantIcon} style={{ height: 100, width: 100, marginRight: 10 }} />
         <View style={styles.container}>
           <Text style={{ fontWeight: "bold", fontSize: 20 }}>{props.item.title}</Text>
           <Row>
@@ -20,18 +38,18 @@ export default function ListItem(props) {
           </Row>
           <Row>
             <Text>Stars: </Text>
-            <Text>{props.item.stars}</Text>
+            {renderStars()}
           </Row>
         </View>
       </Row>
     </TouchableOpacity>
   );
 }
+
 const styles = StyleSheet.create({
   container: {
     display: "flex",
     flex: 1,
     justifyContent: "center",
-
   },
 });
