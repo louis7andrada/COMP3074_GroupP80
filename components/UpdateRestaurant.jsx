ab01253d6feb172f20b961f65f824d49bd2cf125
Jayden Nguyen <jaydennguyen.dev@gmail.com>
2023-12-12 19:08:51 -0500

    feat: implement update feature for new location

--- components/UpdateRestaurant.jsx
+++ components/UpdateRestaurant.jsx
@@ -10,28 +10,35 @@ import {
 import * as SQLite from "expo-sqlite";
 import * as ImagePicker from "expo-image-picker";
 import { Rating } from "react-native-ratings";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+import { useNavigation } from "@react-navigation/native";
+import { useFocusEffect } from "@react-navigation/native";
 
 const db = SQLite.openDatabase("restaurant.db");
 
 export default function UpdateScreen({ route, navigation }) {
 	const { item, updateCallback } = route.params;
 
-	// all updateing states
 	const [updatedName, setUpdatedName] = useState(item.name);
 	const [updatedLocation, setUpdatedLocation] = useState(item.location);
+	const [updatedLatitude, setUpdatedLatitude] = useState(0);
+	const [updatedLongitude, setUpdatedLongitude] = useState(0);
 	const [updatedDescription, setUpdatedDescription] = useState(
 		item.description,
 	);
 	const [updatedImage, setUpdatedImage] = useState(item.image);
 	const [updatedRating, setUpdatedRating] = useState(item.rating);
+	const [mapVisible, setMapVisible] = useState(false);
 
 	useEffect(() => {
+		setUpdatedLocation(JSON.parse(item.location).name);
+		setUpdatedLatitude(JSON.parse(item.location).coordinate.latitude);
+		setUpdatedLongitude(JSON.parse(item.location).coordinate.longitude);
 		setUpdatedImage(item.image);
 		setUpdatedRating(item.rating);
-	}, [item.image, item.rating]);
+	}, [item.location, item.image, item.rating]);
 
 	const updateRestaurant = () => {
-		// error handling if user didn't type sh
 		if (!updatedName || !updatedLocation || !updatedDescription) {
 			alert("Please fill in all the empty fields.");
 			return;
@@ -42,7 +49,13 @@ export default function UpdateScreen({ route, navigation }) {
 				"UPDATE restaurants SET name = ?, location = ?, description = ?, image = ?, rating = ? WHERE id = ?",
 				[
 					updatedName,
-					updatedLocation,
+					JSON.stringify({
+						coordinate: {
+							latitude: updatedLatitude,
+							longitude: updatedLongitude,
+						},
+						name: updatedLocation,
+					}),
 					updatedDescription,
 					updatedImage,
 					updatedRating,
@@ -50,14 +63,9 @@ export default function UpdateScreen({ route, navigation }) {
 				],
 				(txtObj, resultSet) => {
 					alert("Restaurant updated successfully!");
-					// refresh the restaurant list
 					updateCallback();
-
-					// navigate back to the restaurant list
 					navigation.goBack();
 				},
-
-				// error handling
 				(txtObj, error) => {
 					console.log("Error updating restaurant: ", error);
 				},
@@ -65,7 +73,11 @@ export default function UpdateScreen({ route, navigation }) {
 		});
 	};
 
-	// image uploading from Sarah
+	const onMapPress = (e) => {
+		setUpdatedLatitude(e.nativeEvent.coordinate.latitude);
+		setUpdatedLongitude(e.nativeEvent.coordinate.longitude);
+	};
+
 	const uploadImg = async () => {
 		let result = await ImagePicker.launchImageLibraryAsync({
 			allowsEditing: true,
@@ -81,22 +93,15 @@ export default function UpdateScreen({ route, navigation }) {
 
 	return (
 		<View style={styles.container}>
-			{/* heading */}
 			<Text style={styles.heading}>Update Restaurant</Text>
 
-			{/* updateing information */}
 			<TextInput
 				style={styles.input}
 				placeholder='Enter updated name...'
 				value={updatedName}
 				onChangeText={(text) => setUpdatedName(text)}
 			/>
-			<TextInput
-				style={styles.input}
-				placeholder='Enter updated location...'
-				value={updatedLocation}
-				onChangeText={(text) => setUpdatedLocation(text)}
-			/>
+
 			<TextInput
 				style={styles.input}
 				placeholder='Enter updated description...'
@@ -104,12 +109,38 @@ export default function UpdateScreen({ route, navigation }) {
 				onChangeText={(text) => setUpdatedDescription(text)}
 			/>
 
-			{/* button */}
+			<TouchableOpacity
+				style={styles.upBtn}
+				onPress={() => setMapVisible(true)}
+			>
+				<Text style={styles.upText}>Choose new location</Text>
+			</TouchableOpacity>
+
+			{mapVisible && (
+				<MapView
+					provider={PROVIDER_GOOGLE}
+					style={{ height: 200, width: "100%", marginTop: 10 }}
+					region={{
+						latitude: updatedLatitude,
+						longitude: updatedLongitude,
+						latitudeDelta: 0.0022,
+						longitudeDelta: 0.0021,
+					}}
+					onPress={onMapPress}
+				>
+					<Marker
+						coordinate={{
+							latitude: updatedLatitude,
+							longitude: updatedLongitude,
+						}}
+					/>
+				</MapView>
+			)}
+
 			<TouchableOpacity style={styles.upBtn} onPress={uploadImg}>
 				<Text style={styles.upText}>Update Image</Text>
 			</TouchableOpacity>
 
-			{/* update new image */}
 			{updatedImage && (
 				<Image
 					source={{ uri: updatedImage }}
@@ -122,7 +153,6 @@ export default function UpdateScreen({ route, navigation }) {
 				/>
 			)}
 
-			{/* update new ratings */}
 			<Rating
 				style={{
 					alignSelf: "center",
@@ -140,7 +170,6 @@ export default function UpdateScreen({ route, navigation }) {
 				onFinishRating={(value) => setUpdatedRating(value)}
 			/>
 
-			{/* update button */}
 			<TouchableOpacity style={styles.updateBtn} onPress={updateRestaurant}>
 				<Text style={styles.btnText}>Update Restaurant</Text>
 			</TouchableOpacity>
@@ -171,10 +200,11 @@ const styles = StyleSheet.create({
 	upBtn: {
 		borderColor: "grey",
 		borderRadius: 20,
-		width: "30%",
+		width: "100%",
 		backgroundColor: "rgba(217, 217, 217, 0.7)",
-		padding: 10,
+		padding: 15,
 		marginTop: 10,
+		marginBottom: 10,
 		alignSelf: "flex-end",
 	},
 	upText: {
@@ -186,7 +216,6 @@ const styles = StyleSheet.create({
 		backgroundColor: "#50C2C9",
 		padding: 14,
 		borderRadius: 5,
-		// alignSelf: "center",
 		alignItems: "center",
 		width: "100%",
 		height: 50,
